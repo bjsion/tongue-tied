8ecc11e2aaeb6b22d0d8ef5bbb8d5f33c9a644aa
bjsion <bjsion@a6ed42f3-5148-0410-8934-a5afe6022987>
2008-03-25 13:17:38 +0000

    Improved search for keyword flow

--- Server/src/org/tonguetied/web/SearchController.java
+++ Server/src/org/tonguetied/web/SearchController.java
@@ -3,21 +3,16 @@ package org.tonguetied.web;
 import static org.tonguetied.web.Constants.BUNDLES;
 import static org.tonguetied.web.Constants.COUNTRIES;
 import static org.tonguetied.web.Constants.LANGUAGES;
-import static org.tonguetied.web.Constants.SEARCH_FORM;
 import static org.tonguetied.web.Constants.SHOW_ALL;
 import static org.tonguetied.web.Constants.STATES;
-import static org.tonguetied.web.Constants.TRANSLATIONS;
 import static org.tonguetied.web.Constants.VIEW_PREFERENCES;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.SetUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.validation.BindException;
@@ -26,14 +21,14 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.tonguetied.domain.Bundle;
 import org.tonguetied.domain.Country;
-import org.tonguetied.domain.Keyword;
 import org.tonguetied.domain.Language;
-import org.tonguetied.domain.Translation;
 import org.tonguetied.domain.Translation.TranslationState;
 import org.tonguetied.service.ApplicationService;
 
 
 /**
+ * Controller for processing keyword / translation search requests.
+ * 
  * @author bsion
  *
  */
@@ -41,7 +36,7 @@ public class SearchController extends SimpleFormController {
 
     private ApplicationService appService;
     private PreferenceForm viewPreferences;
-    private SearchForm searchForm;
+    private SearchForm searchParameters;
     
     private static final Logger logger = 
         Logger.getLogger(SearchController.class);
@@ -56,7 +51,7 @@ public class SearchController extends SimpleFormController {
     @Override
     protected Object formBackingObject(HttpServletRequest request) 
             throws Exception {
-        return searchForm;
+        return searchParameters;
     }
 
     @Override
@@ -67,34 +62,9 @@ public class SearchController extends SimpleFormController {
         if (logger.isDebugEnabled()) 
             logger.debug("searching for keywords");
         
-        Keyword keyword = searchForm.getKeyword().clone();
-        if (new Translation().equals(keyword.getTranslations().first())) {
-            keyword.setTranslations(SetUtils.EMPTY_SORTED_SET);
-        }
-        
-        List<Keyword> keywords = 
-            appService.findKeywords(keyword,
-                    searchForm.getIgnoreCase(),
-                                    0,
-                                    viewPreferences.getMaxResults());
-
-        List<Translation> translations = 
-            TranslationTransformer.transform(keywords);
-        PreferenceFilter filter = new PreferenceFilter(viewPreferences);
-        CollectionUtils.filter(translations, filter);
-
-        Map<String, Object> model = new HashMap<String, Object>();
-        model.put(TRANSLATIONS, translations);
-        model.put(LANGUAGES, appService.getLanguages());
-        model.put(BUNDLES, appService.getBundles());
-        model.put(COUNTRIES, appService.getCountries());
-        model.put(STATES, TranslationState.values());
-        model.put(SEARCH_FORM, searchForm);
-        model.put(VIEW_PREFERENCES, viewPreferences);
-        
         request.getSession().setAttribute(SHOW_ALL, false);
-        
-        return new ModelAndView(getSuccessView(), model);
+
+        return new ModelAndView(getSuccessView());
     }
 
     @Override
@@ -102,9 +72,11 @@ public class SearchController extends SimpleFormController {
             throws Exception 
     {
         Map<String, Object> model = new HashMap<String, Object>();
+        model.put(LANGUAGES, appService.getLanguages());
         model.put(BUNDLES, appService.getBundles());
         model.put(COUNTRIES, appService.getCountries());
         model.put(STATES, TranslationState.values());
+        model.put(VIEW_PREFERENCES, viewPreferences);
         
         return model;
     }
@@ -141,13 +113,13 @@ public class SearchController extends SimpleFormController {
     public void setViewPreferences(PreferenceForm viewPreferences) {
         this.viewPreferences = viewPreferences;
     }
-
+    
     /**
      * Assign the {@link SearchForm}.
      * 
-     * @param searchForm the searchForm to set
+     * @param searchParameters the searchParameters to set
      */
-    public void setSearchForm(SearchForm searchForm) {
-        this.searchForm = searchForm;
+    public void setSearchParameters(SearchForm searchParameters) {
+        this.searchParameters = searchParameters;
     }
 }
