b48dd7980e3c326e703d460e2f1045040e826299
bjsion <bjsion@a6ed42f3-5148-0410-8934-a5afe6022987>
2008-03-26 15:03:27 +0000

    TT4: Separated embedded server code into its own module distinct from the main application

--- Server/resources/sql/initialData.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO LANGUAGE(code, name) VALUES ('DEFAULT', 'Default(English)');
-INSERT INTO COUNTRY(code, name) VALUES ('DEFAULT', 'Default');
-INSERT INTO BUNDLE(name, description) VALUES ('All', 'This bundle means the keyword applies to all bundles');
-INSERT INTO USER(username, password, firstname, lastname, enabled, email, accountNonExpired, accountNonLocked, credentialsNonExpired) 
-	VALUES('admin', 'admin', 'admin', 'admin', 1, 'none', 1, 1, 1);
-INSERT INTO authorities(userid, permission) VALUES(1, 'ROLE_ADMIN');
