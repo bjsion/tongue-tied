c17376115c3b919fa5eada8f93c57d0331d478ed
bjsion <bjsion@a6ed42f3-5148-0410-8934-a5afe6022987>
2008-04-20 07:39:14 +0000

    TT-24: added support of properties file importer

---
+++ Tests/src/org/tonguetied/datatransfer/Constants.java
@@ -0,0 +1,14 @@
+package org.tonguetied.datatransfer;
+
+import java.io.File;
+
+/**
+ * @author bsion
+ *
+ */
+public interface Constants {
+
+    static final String TEST_DATA_DIR = System.getProperty("user.dir") + 
+        File.separator + "resources" + File.separator + "data" +
+        File.separator;
+}
