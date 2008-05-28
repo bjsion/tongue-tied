8ac8022a79803381c4eec4e771f65ebef8d1268d
bjsion <bjsion@a6ed42f3-5148-0410-8934-a5afe6022987>
2008-05-28 12:39:12 +0000

    renamed default bundle in db

--- trunk/Application/resources/sql/initialData.sql
+++ trunk/Application/resources/sql/initialData.sql
@@ -1,6 +1,6 @@
 INSERT INTO LANGUAGE(code, name) VALUES ('DEFAULT', 'Default(English)');
 INSERT INTO COUNTRY(code, name) VALUES ('DEFAULT', 'Default');
-INSERT INTO BUNDLE(name, resourcename, description, isdefault, isglobal) VALUES ('All', 'All', 'This bundle means the keyword applies to all bundles', true, true);
+INSERT INTO BUNDLE(name, resourcename, description, isdefault, isglobal) VALUES ('All', 'all', 'This bundle means the keyword applies to all bundles', true, true);
 INSERT INTO USER(username, password, firstname, lastname, enabled, email, accountNonExpired, accountNonLocked, credentialsNonExpired) 
 	VALUES('admin', 'admin', 'admin', 'admin', 1, 'none', 1, 1, 1);
 INSERT INTO authorities(userid, permission) VALUES(1, 'ROLE_ADMIN');
