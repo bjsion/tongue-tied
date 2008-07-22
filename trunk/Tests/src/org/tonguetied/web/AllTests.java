127e0a3edb0133f6a96e920c2d3c4d39040a7fc3
bjsion <bjsion@a6ed42f3-5148-0410-8934-a5afe6022987>
2008-07-22 14:07:57 +0000

    TT-38 added ability to change a user's password and update their account profile

--- trunk/Tests/src/org/tonguetied/web/AllTests.java
+++ trunk/Tests/src/org/tonguetied/web/AllTests.java
@@ -15,6 +15,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({
+    AccountFormValidatorTest.class,
     BundleSupportTest.class,
     BundleValidatorTest.class,
     ChangePasswordFormValidatorTest.class,
