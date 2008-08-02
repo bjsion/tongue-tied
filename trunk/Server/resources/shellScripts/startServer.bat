cdcf720d97850ffcf140ad6655eade9690308a95
bjsion <bjsion@a6ed42f3-5148-0410-8934-a5afe6022987>
2008-08-02 07:17:22 +0000

    do not print every action that is taking place

--- trunk/Server/resources/shellScripts/startServer.bat
+++ trunk/Server/resources/shellScripts/startServer.bat
@@ -21,24 +21,24 @@ if defined TT_HOME goto have_tonguetied_home
 
 rem %~dp0 is name of current script under NT
 echo Setting TT_HOME to point to the root tonguetied installation
-set TT_HOME=%~dp0
+@set TT_HOME=%~dp0
 echo TT_HOME = %TT_HOME%
 goto have_tonguetied_home
 
 :have_tonguetied_home
 
-set LIBDIR=%TT_HOME%\libs
-set TT_PATH=%TT_PATH%;%LIBDIR%\server.jar
-set TT_PATH=%TT_PATH%;%LIBDIR%\jetty.jar
-set TT_PATH=%TT_PATH%;%LIBDIR%\jetty-util.jar
-set TT_PATH=%TT_PATH%;%LIBDIR%\servlet-api-2.5.jar
-set TT_PATH=%TT_PATH%;%LIBDIR%\ant.jar
-set TT_PATH=%TT_PATH%;%LIBDIR%\log4j.jar
-set TT_PATH=%TT_PATH%;%LIBDIR%\jsp-2.1.jar
-set TT_PATH=%TT_PATH%;%LIBDIR%\jsp-api-2.1.jar
-set TT_PATH=%TT_PATH%;%LIBDIR%\core-3.1.1.jar
-set TT_PATH=%TT_PATH%;%LIBDIR%\commons-cli-1.1.jar
-set TT_PATH=%TT_PATH%;.
+@set LIBDIR=%TT_HOME%\libs
+@set TT_PATH=%TT_PATH%;%LIBDIR%\server.jar
+@set TT_PATH=%TT_PATH%;%LIBDIR%\jetty.jar
+@set TT_PATH=%TT_PATH%;%LIBDIR%\jetty-util.jar
+@set TT_PATH=%TT_PATH%;%LIBDIR%\servlet-api-2.5.jar
+@set TT_PATH=%TT_PATH%;%LIBDIR%\ant.jar
+@set TT_PATH=%TT_PATH%;%LIBDIR%\log4j.jar
+@set TT_PATH=%TT_PATH%;%LIBDIR%\jsp-2.1.jar
+@set TT_PATH=%TT_PATH%;%LIBDIR%\jsp-api-2.1.jar
+@set TT_PATH=%TT_PATH%;%LIBDIR%\core-3.1.1.jar
+@set TT_PATH=%TT_PATH%;%LIBDIR%\commons-cli-1.1.jar
+@set TT_PATH=%TT_PATH%;.
 
 "%JAVA_HOME%\bin\java" %TT_OPTS% -cp "%TT_PATH%" org.tonguetied.server.Server
 
