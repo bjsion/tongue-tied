571307f94e4ad123c1b8acd1dfae978383f3f8eb
bjsion <bjsion@a6ed42f3-5148-0410-8934-a5afe6022987>
2008-08-02 07:55:33 +0000

    Tidied code

--- trunk/Tests/src/org/tonguetied/datatransfer/exporting/Native2AsciiDirectiveTest.java
+++ trunk/Tests/src/org/tonguetied/datatransfer/exporting/Native2AsciiDirectiveTest.java
@@ -24,11 +24,14 @@ import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
 
 /**
+ * Unit test class to test the custom freemarker directive 
+ * {@link Native2AsciiDirective}.
+ * 
  * @author bsion
- *
+ * 
  */
-public class Native2AsciiDirectiveTest {
-
+public class Native2AsciiDirectiveTest
+{
     private TemplateDirectiveBody body;
     private Environment env;
     private TemplateModel[] loopVars;
@@ -37,19 +40,19 @@ public class Native2AsciiDirectiveTest {
     private Reader in;
     private Template template;
     private TemplateHashModel rootDataModel;
-//    private String expected;
-//    private String input;
 
     /**
      * @throws java.lang.Exception
      */
     @Before
-    public void setUp() throws Exception {
+    public void setUp() throws Exception
+    {
         out = new StringWriter();
         in = new StringReader("test");
         template = new Template("test", in, new Configuration(), null);
+        rootDataModel = null;
         env = new Environment(template, rootDataModel, out);
-        
+
         loopVars = new TemplateModel[] {};
         params = new HashMap<Object, Object>();
     }
@@ -58,53 +61,62 @@ public class Native2AsciiDirectiveTest {
      * @throws java.lang.Exception
      */
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() throws Exception
+    {
         IOUtils.closeQuietly(in);
         IOUtils.closeQuietly(out);
     }
 
     /**
-     * Test method for {@link org.tonguetied.datatransfer.exporting.Native2AsciiDirective#execute(freemarker.core.Environment, java.util.Map, freemarker.template.TemplateModel[], freemarker.template.TemplateDirectiveBody)}.
+     * Test method for
+     * {@link org.tonguetied.datatransfer.exporting.Native2AsciiDirective#execute(freemarker.core.Environment, java.util.Map, freemarker.template.TemplateModel[], freemarker.template.TemplateDirectiveBody)}.
      */
     @Test
-    public final void testExecuteWithAsciiOnly() throws Exception {
+    public final void testExecuteWithAsciiOnly() throws Exception
+    {
         body = new TestBody("abcd");
         Native2AsciiDirective native2Ascii = new Native2AsciiDirective();
         native2Ascii.execute(env, params, loopVars, body);
         StringBuffer buffer = out.getBuffer();
         assertEquals("abcd", buffer.toString());
     }
-    
+
     /**
-     * Test method for {@link org.tonguetied.datatransfer.exporting.Native2AsciiDirective#execute(freemarker.core.Environment, java.util.Map, freemarker.template.TemplateModel[], freemarker.template.TemplateDirectiveBody)}.
+     * Test method for
+     * {@link org.tonguetied.datatransfer.exporting.Native2AsciiDirective#execute(freemarker.core.Environment, java.util.Map, freemarker.template.TemplateModel[], freemarker.template.TemplateDirectiveBody)}.
      */
-    @Test(expected=IllegalArgumentException.class)
-    public final void testExecuteWithNullBody() throws Exception {
+    @Test(expected = IllegalArgumentException.class)
+    public final void testExecuteWithNullBody() throws Exception
+    {
         Native2AsciiDirective native2Ascii = new Native2AsciiDirective();
         native2Ascii.execute(env, params, loopVars, null);
     }
-    
+
     /**
-     * Test method for {@link org.tonguetied.datatransfer.exporting.Native2AsciiDirective#execute(freemarker.core.Environment, java.util.Map, freemarker.template.TemplateModel[], freemarker.template.TemplateDirectiveBody)}.
+     * Test method for
+     * {@link org.tonguetied.datatransfer.exporting.Native2AsciiDirective#execute(freemarker.core.Environment, java.util.Map, freemarker.template.TemplateModel[], freemarker.template.TemplateDirectiveBody)}.
      */
-    @Test(expected=TemplateModelException.class)
-    public final void testExecuteWithInvalidParams() throws Exception {
+    @Test(expected = TemplateModelException.class)
+    public final void testExecuteWithInvalidParams() throws Exception
+    {
         params.put("key", "value");
         Native2AsciiDirective native2Ascii = new Native2AsciiDirective();
         native2Ascii.execute(env, params, loopVars, body);
     }
-    
+
     /**
-     * Test method for {@link org.tonguetied.datatransfer.exporting.Native2AsciiDirective#execute(freemarker.core.Environment, java.util.Map, freemarker.template.TemplateModel[], freemarker.template.TemplateDirectiveBody)}.
+     * Test method for
+     * {@link org.tonguetied.datatransfer.exporting.Native2AsciiDirective#execute(freemarker.core.Environment, java.util.Map, freemarker.template.TemplateModel[], freemarker.template.TemplateDirectiveBody)}.
      */
-    @Test(expected=TemplateModelException.class)
-    public final void testExecuteWithInvalidLoopVars() throws Exception {
-        loopVars = new TemplateModel[] {null, null};
+    @Test(expected = TemplateModelException.class)
+    public final void testExecuteWithInvalidLoopVars() throws Exception
+    {
+        loopVars = new TemplateModel[] { null, null };
         Native2AsciiDirective native2Ascii = new Native2AsciiDirective();
         native2Ascii.execute(env, params, loopVars, body);
     }
 
-    private static final class TestBody implements TemplateDirectiveBody 
+    private static final class TestBody implements TemplateDirectiveBody
     {
         private String input;
 
@@ -113,11 +125,13 @@ public class Native2AsciiDirectiveTest {
          * 
          * @param input
          */
-        public TestBody(String input) {
+        public TestBody(String input)
+        {
             this.input = input;
         }
 
-        public void render(Writer newOut) throws IOException {
+        public void render(Writer newOut) throws IOException
+        {
             newOut.write(input);
         }
     }
