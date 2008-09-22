cca1edccdc6fb1b49411687f80cfdbd18b75ae2d
bjsion <bjsion@a6ed42f3-5148-0410-8934-a5afe6022987>
2008-09-22 14:21:41 +0000

    TT-25 Added license header to source files

--- Application/src/org/tonguetied/datatransfer/DataServiceImpl.java
+++ Application/src/org/tonguetied/datatransfer/DataServiceImpl.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008 The Tongue-Tied Authors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations 
+ * under the License. 
+ */
 package org.tonguetied.datatransfer;
 
 import static fmpp.setting.Settings.NAME_DATA;
@@ -46,10 +61,13 @@ import fmpp.setting.SettingException;
 import fmpp.setting.Settings;
 
 /**
+ * Concrete implementation of the {@link DataService} interface.
+ * 
  * @author bsion
  *
  */
-public class DataServiceImpl implements DataService {
+public class DataServiceImpl implements DataService
+{
     private Settings settings;
     private TransferRepository transferRepository;
     private KeywordService keywordService;
--- Application/src/org/tonguetied/usermanagement/AuthenticationException.java
+++ Application/src/org/tonguetied/usermanagement/AuthenticationException.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2008 The Tongue-Tied Authors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations 
+ * under the License. 
+ */
 package org.tonguetied.usermanagement;
 
 /**
@@ -13,7 +28,7 @@ public class AuthenticationException extends RuntimeException
     /**
      * Create a new instance of AuthenticationException.
      * 
-     * @param message TODO
+     * @param message the message of the error
      */
     public AuthenticationException(String message) {
         super(message);
