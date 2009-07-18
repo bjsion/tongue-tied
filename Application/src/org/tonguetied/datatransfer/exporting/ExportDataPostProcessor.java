b83be0c2796e90bb9fcb9ef3b403bb9476ad3956
bjsion <bjsion@a6ed42f3-5148-0410-8934-a5afe6022987>
2009-07-18 06:55:18 +0000

    Tag release 1.2.1

---
+++ Application/src/org/tonguetied/datatransfer/exporting/ExportDataPostProcessor.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2008 The Tongue-Tied Authors
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not 
+ * use this file except in compliance with the License. You may obtain a copy 
+ * of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations 
+ * under the License. 
+ */
+package org.tonguetied.datatransfer.exporting;
+
+import java.util.List;
+import java.util.Map;
+
+import org.tonguetied.datatransfer.common.ExportParameters;
+import org.tonguetied.keywordmanagement.Country;
+import org.tonguetied.keywordmanagement.Translation;
+
+/**
+ * Data post processor that performs transformations / formatting of data
+ * retrieved from persistence.
+ * 
+ * @author bsion
+ * 
+ */
+public interface ExportDataPostProcessor
+{
+
+    /**
+     * Using an existing list of {@link Translation}s, transform this list into
+     * another list containing a different view of the same data in
+     * <code>translations</code>.
+     * 
+     * @param translations the list of {@link Translation}s to transform
+     * @param defaultCountry an instance of the default {@link Country}
+     * @return List of objects that may or may not be of type
+     *         {@link Translation} that is a different representation of the
+     *         <code>translations</code>
+     */
+    List<?> transformData(List<Translation> translations, final Country defaultCountry);
+
+    /**
+     * Add additional data used by the template to format the export data
+     * 
+     * @param root the root level object passed to the templating library
+     * @param parameters the {@link ExportParameters} used to generate this
+     *        export
+     */
+    void addData(Map<String, Object> root, ExportParameters parameters);
+}
